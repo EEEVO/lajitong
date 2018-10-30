4e070973b50186e78e73e2d9770b3f04027e8108
肖锦 <100355058@qq.com>
2018-10-30 14:33:15 +0800

    首页

--- users/mini-program/src/pages/index/index.vue
+++ users/mini-program/src/pages/index/index.vue
@@ -1,63 +1,178 @@
 <template>
     <div class="page-index">
-        <i-button
-            size="small"
-            type="primary"
-            @click="http">http</i-button>
-        <i-button
-            size="small"
-            type="primary"
-            @click="jump">jump</i-button>
-        <i-button
-            size="small"
-            type="primary"
-            @click="toast">toast</i-button>
-        <i-button
-            size="small"
-            type="primary"
-            @click="showloading">showloading</i-button>
-        <i-button
-            size="small"
-            type="primary"
-            @click="hideloading">hideloading</i-button>
+        <swiper class="swiper-content">
+            <swiper-item class="swiper-slide">
+                <div class="swiper-slide-img-wrapper">
+                    <image mode="aspectFit" class="slide-image" src="https://dummyimage.com/700x300/f5d639/fff"></image>
+                </div>
+            </swiper-item>
+            <swiper-item class="swiper-slide">
+                <div class="swiper-slide-img-wrapper">
+                    <image mode="aspectFit" class="slide-image" src="https://dummyimage.com/700x300/f5d639/fff"></image>
+                </div>
+            </swiper-item>
+            <swiper-item class="swiper-slide">
+                <div class="swiper-slide-img-wrapper">
+                    <image mode="aspectFit" class="slide-image" src="https://dummyimage.com/700x300/f5d639/fff"></image>
+                </div>
+            </swiper-item>
+        </swiper>
+        <div class="nav-list">
+            <div class="nav-item" v-for="(item, index) in navList" :key="index">
+                <img :src="item.icon" alt="">
+                <h4>{{item.title}}</h4>
+            </div>
+        </div>
+        <div class="card-list">
+            <div class="card-item card-green-gold">
+                <div class="title">
+                    <h5>我的环保金</h5>
+                    <p>{{greenGold}} <span>元</span></p>
+                </div>
+                <div class="link">我的钱包<div class="icon-wrapper"><i-icon color="#666" type="enter" /></div></div>
+            </div>
+            <div class="card-item card-count">
+                <div class="title">
+                    <h5>累计投递</h5>
+                    <p>{{count}} <span>次</span></p>
+                </div>
+                <div class="link">查看记录<div class="icon-wrapper"><i-icon color="#666" type="enter" /></div></div>
+            </div>
+        </div>
+        <div class="code"><div class="icon-wrapper"><i-icon size="20" color="#000" type="scan" /></div><div class="txt">生成二维码投递</div></div>
     </div>
 </template>
 
 <script>
     import { isForbidden } from "utils/stopRequest";
-    import API from 'api/apiList'
 
     export default {
-        async created() {
-
-        },
-        methods: {
-            async http() {
-                if (isForbidden()) return;
-                const data = this.$get(API.baseInfo);
-                console.log(data);
-            },
-            jump() {
-                this.$router.push({
-                    path: "/pages/demo/main"
-                });
-            },
-            toast() {
-                this.$toast('嗨~')
-            },
-            showloading() {
-                this.$showloading()
-                setTimeout(() => {
-                    this.$hideloading()
-                }, 2000)
-            },
-            hideloading() {
-                this.$hideloading()
+        data() {
+            return {
+                navList: [{
+                    title: '附近回收机',
+                    icon: require('../../assets/imgs/icon-local.png')
+                },{
+                    title: '帮助中心',
+                    icon: require('../../assets/imgs/icon-help.png')
+                },{
+                    title: '个人中心',
+                    icon: require('../../assets/imgs/icon-user.png')
+                }],
+                greenGold: '0.00',
+                count: 0
             }
         }
     };
 </script>
 
-<style scoped>
+<style scoped lang="less">
+    @import "../../styles/mixin";
 
+    .page-index {
+        padding-top: 10px;
+        .swiper-content {
+            height: 150px;
+            .swiper-slide {
+                .swiper-slide-img-wrapper {
+                    width: 93%;
+                    height: 150px;
+                    border-radius: 6px;
+                    overflow: hidden;
+                    margin: 0 auto;
+                }
+                .slide-image {
+                    width: 100%;
+                    height: 150px;
+                }
+            }
+
+        }
+        .nav-list {
+            display: flex;
+            padding: 20px 0;
+            .nav-item {
+                flex: 1;
+                text-align: center;
+                h4 {
+                    font-size: @fs14;
+                    text-align: center;
+                    color: @fontColor3;
+                }
+                img {
+                    width: 32px;
+                    height: 32px;
+                    display: block;
+                    margin: 0 auto 10px;
+                }
+            }
+        }
+        .icon-wrapper {
+            position: relative;
+            top: -1px;
+        }
+        .card-list {
+            .card-item {
+                box-shadow: 0 0 4px rgba(0,0,0,.1);
+                width: 93%;
+                margin: 20px auto 0;
+                border-radius: 6px;
+                padding: 15px;
+                display: flex;
+                position: relative;
+                .title {
+                    h5 {
+                        font-size: @fs14;
+                        color: @fontColor6;
+                    }
+                    p {
+                        font-size: @fs30;
+                        color: #000;
+                        margin-top: 10px;
+                        font-weight: bold;
+                        span {
+                            font-size: @fs16;
+                        }
+                    }
+                }
+                .link {
+                    position: absolute;
+                    top: 50%;
+                    right: 15px;
+                    transform: translate(0, -50%);
+                    font-size: @fs14;
+                    color: @fontColor9;
+                    border: 1rpx solid @fontColor9;
+                    padding: 4px 10px;
+                    border-radius: 20px;
+                    display: flex;
+                    align-items: center;
+                    &:active {
+                        opacity: .8;
+                    }
+                }
+            }
+        }
+        .code {
+            padding: 10px 20px;
+            background: rgba(245, 215, 57, 0.8);
+            background: linear-gradient(rgba(245, 215, 57, 0.8),rgba(245, 215, 57, 1));
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 30px auto 0;
+            width: 220px;
+            border-radius: 30px;
+            box-shadow: 0 0 4px rgba(0,0,0,.1);
+            &:active {
+                opacity: .8;
+            }
+            .txt {
+                margin-left: 6px;
+            }
+            .icon-wrapper {
+                top: 0;
+            }
+        }
+    }
 </style>
---
+++ users/mini-program/src/pages/test/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="page-index">
+        <i-button
+            size="small"
+            type="primary"
+            @click="http">http</i-button>
+        <i-button
+            size="small"
+            type="primary"
+            @click="jump">jump</i-button>
+        <i-button
+            size="small"
+            type="primary"
+            @click="toast">toast</i-button>
+        <i-button
+            size="small"
+            type="primary"
+            @click="showloading">showloading</i-button>
+        <i-button
+            size="small"
+            type="primary"
+            @click="hideloading">hideloading</i-button>
+    </div>
+</template>
+
+<script>
+    import { isForbidden } from "utils/stopRequest";
+    import API from 'api/apiList'
+
+    export default {
+        async created() {
+
+        },
+        methods: {
+            async http() {
+                if (isForbidden()) return;
+                const data = this.$get(API.baseInfo);
+                console.log(data);
+            },
+            jump() {
+                this.$router.push({
+                    path: "/pages/demo/main"
+                });
+            },
+            toast() {
+                this.$toast('嗨~')
+            },
+            showloading() {
+                this.$showloading()
+                setTimeout(() => {
+                    this.$hideloading()
+                }, 2000)
+            },
+            hideloading() {
+                this.$hideloading()
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
