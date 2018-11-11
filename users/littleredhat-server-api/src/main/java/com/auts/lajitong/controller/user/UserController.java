947eb1f88bd845c90b0329ffbf3fa94538d45a1b
黄荣卫 <rongwei.huang@phicomm.com>
2018-11-11 21:13:38 +0800

    后台api-增加修改属性假接口

--- users/littleredhat-server-api/src/main/java/com/auts/lajitong/controller/user/UserController.java
+++ users/littleredhat-server-api/src/main/java/com/auts/lajitong/controller/user/UserController.java
@@ -4,23 +4,20 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.auts.lajitong.consts.Const;
 import com.auts.lajitong.controller.SBaseController;
 import com.auts.lajitong.model.common.PhiHomeBaseResponse;
-import com.auts.lajitong.model.enums.OrderStatus;
-import com.auts.lajitong.model.response.AllInfoCountResponse;
-import com.auts.lajitong.service.OrderService;
 
 /**
- * 用户相关功能
+ * 用户个人信息接口.
  *
- * @author li.bing
+ * rongwei.huang
+ *
  */
 @RestController
 @CrossOrigin
@@ -31,36 +28,18 @@ public class UserController extends SBaseController {
     public static String HTTP_HEAD_AUTHORIZATION = "Authorization";
     public static String HTTP_HEAD_CONTENT_TYPE = "Content-Type";
 
-    @Autowired
-    OrderService orderService;
-
     /**
-     * 查询产品列表
-     * type 01：集合信托  02集合资管 03债权基金 04股权基金 05阳光私募
+     * 修改个人信息.
      * @param request
      * @return
      */
-    @RequestMapping(value = "/v1/user/allInfoCount", method = RequestMethod.GET, produces = { "application/json" })
-    public PhiHomeBaseResponse queryAllInfoCount(HttpServletRequest request) {
+    @RequestMapping(value = "/users/v1/modify", method = RequestMethod.POST, produces = { "application/json" })
+    public PhiHomeBaseResponse queryAllInfoCount(HttpServletRequest request,
+            @RequestParam(value = "id", required = true) String id,
+            @RequestParam(value = "nickName", required = true) String nickName,
+            @RequestParam(value = "sex", required = true) int sex) {
         PhiHomeBaseResponse rspObj = new PhiHomeBaseResponse();
-        AllInfoCountResponse allInfoCount = new AllInfoCountResponse();
-        LOGGER.info("queryAllInfoCount type [{}]");
-        String token = request.getHeader(Const.AUTHORIZATION);
-        LOGGER.info("queryOrders toekn [{}]", token);
-        String uid = getUidByToken(token);
 
-        //计算佣金 todo ....
-        int totalCount = orderService.queryOrderCountByStatus(null, uid);
-        int wpTotalCount = orderService.queryOrderCountByStatus(OrderStatus.WP.getValue(), uid);
-        int wcTotalCount = orderService.queryOrderCountByStatus(OrderStatus.WC.getValue(), uid);
-        int ocTotalCount = orderService.queryOrderCountByStatus(OrderStatus.OC.getValue(), uid);
-        int ofTotalCount = orderService.queryOrderCountByStatus(OrderStatus.OF.getValue(), uid);
-        allInfoCount.getStatusMap().put(OrderStatus.WP.getValue(), wpTotalCount);
-        allInfoCount.getStatusMap().put(OrderStatus.WC.getValue(), wcTotalCount);
-        allInfoCount.getStatusMap().put(OrderStatus.OC.getValue(), ocTotalCount);
-        allInfoCount.getStatusMap().put(OrderStatus.OF.getValue(), ofTotalCount);
-        allInfoCount.getStatusMap().put("ALL", totalCount);
-        rspObj.setResult(allInfoCount);
         return successResponse(rspObj);
     }
 }
