0064fb143b8834952d5de3734a51fe4184b34256
lb <lb47917@LB47917.tcent.cn>
2018-11-21 17:55:02 +0800

    --api device

--- users/littleredhat-server-api/src/main/java/com/auts/lajitong/dao/DeviceBinWatcherMapper.java
+++ users/littleredhat-server-api/src/main/java/com/auts/lajitong/dao/DeviceBinWatcherMapper.java
@@ -1,10 +1,19 @@
 package com.auts.lajitong.dao;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+
 import com.auts.lajitong.model.dao.device.DeviceBinWatcherModel;
 
 public interface DeviceBinWatcherMapper {
     int deleteByPrimaryKey(Long id);
 
+    @Insert("insert into tbl_bin_watcher (f_id, device_id,sort_no, volumn, sszl, wd, work_status, "
+    		+ "error_code, error_msg, create_time, update_time) "
+            + "values (#{record.fId},#{record.deviceId}, #{record.sortNo},#{record.volumn},#{record.sszl},#{record.wd},#{record.workStatus}, "
+            + "#{record.errorCode},#{record.errorMsg},#{record.createTime},#{record.updateTime})"
+            )
+    @Options(useGeneratedKeys = true, keyProperty = "record.id")
     int insert(DeviceBinWatcherModel record);
 
     int insertSelective(DeviceBinWatcherModel record);
--- users/littleredhat-server-api/src/main/java/com/auts/lajitong/dao/DeviceWatcherMapper.java
+++ users/littleredhat-server-api/src/main/java/com/auts/lajitong/dao/DeviceWatcherMapper.java
@@ -1,10 +1,23 @@
 package com.auts.lajitong.dao;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+
 import com.auts.lajitong.model.dao.device.DeviceWatcherModel;
 
 public interface DeviceWatcherMapper {
     int deleteByPrimaryKey(Long id);
 
+    @Insert("insert into tbl_device_watcher (device_id, gps_lng, gps_lat, signal_intensit, firmware_version, "
+    		+ "  charing_voltage, charing_current, daily_charge, daily_electricity, mainboard_temperature,  "
+    	    + "  battery_temperature, battery_voltage, sort_sum,  mainboard_id, ws_time, capacity,  "
+    	    + "  weight, temperatur, water_line, create_time, update_time) "
+            + "values (#{record.deviceId},#{record.gpsLng}, #{record.gpsLat},#{record.signalIntensit},#{record.firmwareVersion}, "
+    	    + "#{record.charingVoltage},#{record.charingCurrent},#{record.dailyCharge},#{record.dailyElectricity},#{record.mainboardTemperature},"
+    	    + "#{record.batteryTemperature},#{record.batteryVoltage},#{record.sortSum},#{record.mainboardId},#{record.wsTime},#{record.capacity},"
+            + "#{record.weight},#{record.temperatur},#{record.waterLine},#{record.createTime},#{record.updateTime})"
+            )
+    @Options(useGeneratedKeys = true, keyProperty = "record.id")
     int insert(DeviceWatcherModel record);
 
     int insertSelective(DeviceWatcherModel record);
