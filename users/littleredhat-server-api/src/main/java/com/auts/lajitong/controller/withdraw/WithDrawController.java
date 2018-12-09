f1f426288316ef50e89997bab100663408d30e34
黄荣卫 <huangrongwei@cmhi.chinamobile.com>
2018-12-09 12:00:21 +0800

    后台api-增加余额明细接口

--- users/littleredhat-server-api/src/main/java/com/auts/lajitong/controller/withdraw/WithDrawController.java
+++ users/littleredhat-server-api/src/main/java/com/auts/lajitong/controller/withdraw/WithDrawController.java
@@ -1,20 +1,30 @@
 package com.auts.lajitong.controller.withdraw;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.auts.lajitong.consts.Const;
 import com.auts.lajitong.controller.SBaseController;
+import com.auts.lajitong.controller.user.UserController;
 import com.auts.lajitong.model.common.PhiHomeBaseResponse;
 import com.auts.lajitong.model.dao.WithdrawModel;
+import com.auts.lajitong.model.response.litteredhat.LitteredBalanceListResponseModel;
+import com.auts.lajitong.model.response.litteredhat.LitteredBalanceListResponseModel.BalanceData;
 import com.auts.lajitong.service.WithdrawService;
+import com.auts.lajitong.util.MyListUtils;
 import com.auts.lajitong.util.StringUtil;
 
 /**
@@ -26,24 +36,48 @@ import com.auts.lajitong.util.StringUtil;
 @CrossOrigin
 public class WithDrawController extends SBaseController {
 
+    private static final Logger LOGGER = LogManager.getLogger(WithDrawController.class);
+
     @Autowired
     WithdrawService withdrawService;
 
     /**
-     * 提现记录查询.
+     * 余额明细查询，微信支付宝提现，订单收入.
+     *
      * @param request
      * @return
      */
-    @RequestMapping(value = "v1/with_draw_history", method = RequestMethod.GET, produces = { "application/json" })
-    public PhiHomeBaseResponse getBanner(HttpServletRequest request) {
-        PhiHomeBaseResponse rsp = new PhiHomeBaseResponse();
-        String userId = request.getHeader(Const.AUTHORIZATION);
-        if (StringUtil.isNullOrEmpty(userId)) {
-            return errorResponse(Const.ErrorCode.REQUEST_NO_PARAS);
-        }
-        List<WithdrawModel> models = withdrawService.getByUserId(userId);
-        rsp.setResult(models);
+    @RequestMapping(value = "/v1/balance/lists", method = RequestMethod.GET, produces = { "application/json" })
+    public PhiHomeBaseResponse getBalanceList(HttpServletRequest request,
+            @RequestParam(value = "userId", required = true) String userId) {
+//        String userId = request.getHeader(Const.AUTHORIZATION);
+//        if (StringUtil.isNullOrEmpty(userId)) {
+//            return errorResponse(Const.ErrorCode.REQUEST_NO_PARAS);
+//        }
+        LOGGER.info("getBalanceList userId: [{}]", userId);
 
-        return successResponse(rsp);
+        List<BalanceData> lists = new ArrayList<>();
+
+        List<WithdrawModel> models = withdrawService.getByUserId(userId);
+        if (!MyListUtils.isEmpty(models)) {
+            for (WithdrawModel withdrawModel : models) {
+                BalanceData rspBalanceModel = new BalanceData();
+                rspBalanceModel.setAmount(withdrawModel.getAmount());
+                rspBalanceModel.setType(withdrawModel.getWithdrawType());
+                rspBalanceModel.setOperate_time(transTime(withdrawModel.getCreateTime()));
+                lists.add(rspBalanceModel);
+            }
+        }
+        LitteredBalanceListResponseModel bannerRspModels = new LitteredBalanceListResponseModel();
+        bannerRspModels.setData(lists);
+
+        PhiHomeBaseResponse rspObj = new PhiHomeBaseResponse();
+        rspObj.setResult(bannerRspModels);
+        return successResponse(rspObj);
+    }
+
+    private String transTime(long time){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(new Date(time));
     }
 }
