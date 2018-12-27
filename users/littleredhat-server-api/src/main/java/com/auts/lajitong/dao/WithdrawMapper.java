c7178edf73e569c1c6ba7a513cf603011f8467ac
lb <lb47917@LB47917.tcent.cn>
2018-12-27 15:39:05 +0800

    --update api withdraw

--- users/littleredhat-server-api/src/main/java/com/auts/lajitong/dao/WithdrawMapper.java
+++ users/littleredhat-server-api/src/main/java/com/auts/lajitong/dao/WithdrawMapper.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import com.auts.lajitong.model.dao.WithdrawModel;
 
@@ -13,6 +15,16 @@ import com.auts.lajitong.model.dao.WithdrawModel;
 public interface WithdrawMapper {
 
     @Select("select * from tbl_withdraw where user_id=#{userId} and status=0")
+    @Results({
+    	@Result(property = "id", column = "id"),
+    	@Result(property = "userId", column = "user_id"),
+    	@Result(property = "amount", column = "amount"),
+    	@Result(property = "withdrawType", column = "withdraw_type"),
+    	@Result(property = "orderNo", column = "order_no"),
+    	@Result(property = "status", column = "status"),
+    	@Result(property = "reason", column = "reason"),
+    	@Result(property = "createTime", column = "create_time")
+    })
     List<WithdrawModel> getByUid(@Param("userId") String userId);
 
     @Insert("insert into tbl_withdraw(user_id, amount, withdraw_type, order_no, status, reason, create_time) "
