cabfea0cf82300ac27e3de1e0bbc3eaa97170e3b
lb <lb47917@LB47917.tcent.cn>
2018-12-29 11:25:25 +0800

    --update

--- users/littleredhat-server-api/src/main/java/com/auts/lajitong/controller/user/UserController.java
+++ users/littleredhat-server-api/src/main/java/com/auts/lajitong/controller/user/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
 import com.auts.lajitong.consts.Const;
 import com.auts.lajitong.controller.SBaseController;
 import com.auts.lajitong.model.common.PhiHomeBaseResponse;
@@ -97,13 +98,13 @@ public class UserController extends SBaseController {
         try {
         	List<LitteredHatDeliveryListResponse.DeliverModel> data = new ArrayList<>();
             List<LitteredhatDeliveryListModel> models = deliveryService.getListByUser(userId);
+            LOGGER.info("queryDelivers result:" + JSON.toJSONString(models));
             if (!MyListUtils.isEmpty(models)) {
                 for (int i = 0; i < models.size(); i++) {
                     LitteredhatDeliveryListModel mm = models.get(i);
                     convertDTO(mm);
-                    LitteredHatDeliveryListResponse.DeliverModel rspMM = new LitteredHatDeliveryListResponse.DeliverModel(
-                            mm);
-
+                    LitteredHatDeliveryListResponse.DeliverModel rspMM = new LitteredHatDeliveryListResponse.DeliverModel(mm);
+                    LOGGER.info("queryDelivers result:" + JSON.toJSONString(rspMM));
                     data.add(rspMM);
                 }
             }
