7513cc504a8c45ed9cdf93d1c2d2ce019cfdc565
lb <lb47917@LB47917.tcent.cn>
2019-01-07 11:29:17 +0800

    --admin 设备管理update

--- users/littleredhat-server-backend/src/main/java/com/auts/ljtmanager/dao/DeviceBinWatcherMapper.java
+++ users/littleredhat-server-backend/src/main/java/com/auts/ljtmanager/dao/DeviceBinWatcherMapper.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 import com.auts.ljtmanager.model.dao.device.DeviceBinWatcherModel;
 
@@ -23,7 +26,22 @@ public interface DeviceBinWatcherMapper {
 
     DeviceBinWatcherModel selectByPrimaryKey(Long id);
     
-    List<DeviceBinWatcherModel> selectDeviceBinByFId(Long id);
+    @Select("SELECT * FROM tbl_bin_watcher a WHERE #{fId} = a.f_id")
+    @Results({
+    	@Result(property = "id", column = "id"),
+    	@Result(property = "fId", column = "f_id"),
+    	@Result(property = "deviceId", column = "device_id"),
+    	@Result(property = "sortNo", column = "sort_no"),
+    	@Result(property = "volumn", column = "volumn"),
+    	@Result(property = "sszl", column = "sszl"),
+    	@Result(property = "wd", column = "wd"),
+    	@Result(property = "workStatus", column = "work_status"),
+    	@Result(property = "errorCode", column = "error_code"),
+    	@Result(property = "errorMsg", column = "error_msg"),
+    	@Result(property = "createTime", column = "create_time"),
+    	@Result(property = "updateTime", column = "update_time")
+    })
+    List<DeviceBinWatcherModel> selectDeviceBinByFId(@Param("fId") Long id);
 
     int updateByPrimaryKeySelective(DeviceBinWatcherModel record);
 
